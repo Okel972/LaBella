d342d52d8eb02a951a04386b3db45efa4d44b948
tweekdev <tja.theau54@gmail.com>
2023-11-17 13:09:10 +0100

    add image veggie

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,13 +1,10 @@
 import './Carousel.css';
-import { useState } from 'react';
 import ImageGallery from 'react-image-gallery';
 
 const images = [
   {
-    original: 'img/Slide_1.jpg',
-    thumbnail: 'img/Slide_1.jpg',
-    title: 'la veggie',
-    descriptionImage: 'Une nouvelle offre végétarienne.',
+    original: 'img/veggie.png',
+    thumbnail: 'img/veggie.png',
   },
   {
     original: 'img/Slide_2.jpg',
@@ -24,31 +21,14 @@ const images = [
 ];
 
 export const Carousel = () => {
-  // Use state to keep track of the active index
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  // Event handler for updating the active index
-  const handleSlide = index => {
-    setCurrentIndex(index);
-  };
-
   return (
-    <div>
-      <ImageGallery
-        className="carousel"
-        items={images}
-        showPlayButton={false}
-        showFullscreenButton={false}
-        showThumbnails={false}
-        autoPlay={false}
-        onSlide={index => handleSlide(index)}
-      />
-      {currentIndex === 0 && images.length > 0 && (
-        <div>
-          <span class="title">{images[0].title}</span>
-          <span class="description">{images[0].descriptionImage}</span>
-        </div>
-      )}
-    </div>
+    <ImageGallery
+      className="carousel"
+      items={images}
+      showPlayButton={false}
+      showFullscreenButton={false}
+      showThumbnails={false}
+      autoPlay={false}
+    />
   );
 };
